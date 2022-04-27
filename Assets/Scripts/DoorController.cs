2733e72c300d331f1a5350faa7e17a0e79d00886
Robert <chen1fan@icloud.com>
2022-04-27 13:37:44 -0400

    add last progression implement sound track

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -12,10 +12,13 @@ public class DoorController : MonoBehaviour
     [SerializeField] private MeshRenderer ScannerwaveRenderer;
     [SerializeField] private Color TransparentGreen;
     [SerializeField] private Color TransparentBlue;
+
+    [SerializeField] private GameObject[] indicateLight;
+    
     // Start is called before the first frame update
     void Start()
     {
-        if (On)
+        if (On&&doorRenderer != null)
         {
             doorRenderer.materials[1].EnableKeyword("_EMISSION");
             doorRenderer.materials[1].SetColor("_EmissionColor", Color.green*3.0f);
@@ -25,7 +28,7 @@ public class DoorController : MonoBehaviour
             }
             
         }
-        else
+        else if(doorRenderer != null)
         {
             doorRenderer.materials[1].SetColor("_EmissionColor", Color.black);
             if (Scanner != null)
@@ -48,6 +51,8 @@ public class DoorController : MonoBehaviour
         {
         doorAnimator.SetBool("Open",true);
         open = true;
+
+        GetComponent<AudioSource>().PlayOneShot(GetComponent<AudioSource>().clip);
         }
         
     }
@@ -80,5 +85,40 @@ public class DoorController : MonoBehaviour
     public void ScannerUndetect()
     {
         ScannerwaveRenderer.material.SetColor("_Color", TransparentBlue);
+    }
+
+    public void TrippleGateEnter(int scannerCode)
+    {
+        IndicateLightColorChange(indicateLight[scannerCode], Color.green);
+
+        bool open = true;
+        /*
+        for (int i = 0; i < 3; i++)
+        {
+            if (indicateLight[i].GetComponent<MeshRenderer>().materials[1].GetColor("_Color") != Color.green)
+            {
+                open = false;
+                break;
+            }
+        }
+        */
+
+        if (open)
+        {
+            OpenDoor();
+        }
+    }
+
+    public void TrippleGateExit(int scannerCode)
+    {
+        IndicateLightColorChange(indicateLight[scannerCode], Color.red);
+    }
+
+    public void IndicateLightColorChange(GameObject light,Color color)
+    {
+        Debug.Log("change light");
+        light.GetComponent<MeshRenderer>().materials[1].SetColor("_Color",color);
+        light.GetComponent<MeshRenderer>().materials[1].SetColor("_EmissionColor", color*1.5f);
+        light.transform.GetChild(0).gameObject.GetComponent<Light>().color = color;
     }
 }
